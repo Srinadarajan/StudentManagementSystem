52d73a40489f7b7bedb1f41bfba47a8744fa162b
Srinadarajan <kethikasrinadarajan@gmail.com>
2020-07-22 16:08:21 +0530

    comments01

--- src/student/ViewStudent.js
+++ src/student/ViewStudent.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react'
 import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
+import '../assets/style.css'
 
 export class ViewStudent extends Component {
   render() {
     return (
-      <div>
+      <div id ="table">
         <Table striped bordered hover>
           <thead>
             <tr>
               <th>#</th>
               <th>First Name</th>
               <th>Last Name</th>
-             </tr>
+              <th>NIC</th>
+              <th>Date of Birth</th>
+              <th>Email</th>
+              <th>Stream</th>
+              <th>Gender</th>
+              <th>View</th>
+            </tr>
           </thead>
           <tbody>
             <tr>
               <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
+              <th>Srinadarajan</th>
+              <th>Nadarajah</th>
+              <th>123456789V</th>
+              <th>21 June 1995</th>
+              <th>sri@gmail.com</th>
+              <th>Bio</th>
+              <th>Male</th>
+              <th><Button variant="primary"> View </Button></th>
             </tr>
             <tr>
               <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
+              <th>Sri</th>
+              <th>Mala</th>
+              <th>123456780V</th>
+              <th>30 May 1998</th>
+              <th>sri@gmail.com</th>
+              <th>Arts</th>
+              <th>Female</th>
+              <th><Button variant="primary"> View </Button></th>
              </tr>
           </tbody>
         </Table>
